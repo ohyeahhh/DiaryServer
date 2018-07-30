94e7097faa9958d61ea7dcec18474a1298035337
ohyeah <ohyeahmisoh@gmail.com>
2018-07-30 14:53:10 +0800

    更正路径

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(api);
 app.use(express.static(path.resolve(__dirname, './dist')))
 // 因为是单页应用 所有请求都走/dist/index.html
 app.get('*', function(req, res) {
-    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
     res.send(html)
 })
 // 监听8088端口
