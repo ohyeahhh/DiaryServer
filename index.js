b20b2fb23ae3448fa002591bb5eff913d3090c50
ohyeah <ohyeahmisoh@gmail.com>
2018-10-08 23:25:10 +0800

    修复node 服务器路径错误

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(api);
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
-app.use(express.static(path.resolve(__dirname, './dist')))
+app.use(express.static(path.resolve(__dirname, '../dist')))
 // 因为是单页应用 所有请求都走/dist/index.html
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
     res.send(html)
 })
